0113f34032cb28005300dce7a8e9c198ea0eb448
lanbery <cwd-pc@xy-info.com>
2025-09-15 10:10:33 +0800

    ci: unit test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,1 @@
-/**
- * Perform the sum of the provided numbers
- * @param numbers - infinite list of numbers to sum
- * @returns
- */
-
-export const sum = (...numbers: number[]): number =>
-  numbers.reduce((previousValue, currentValue) => currentValue + previousValue);
+export * as CommonUtils from './common-utils';
