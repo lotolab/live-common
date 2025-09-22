2fe9d03ca9a9ce228d8be6471a745324db8e3dfc
lanbery <lanbery@gmail.com>
2025-09-23 00:18:03 +0800

    feat: add ui tyes

--- src/types/ui.types.ts
+++ src/types/ui.types.ts
@@ -5,3 +5,41 @@ export type SelectionItem = {
   extra?: any;
   [k: string]: any;
 };
+
+/**
+ * PTL Position Top Left
+ * PMC Position Middle Center
+ * BMSG Barrage Message 底部到第二行剧中
+ * PBR Position Bootom
+ *
+ */
+export type PositionType =
+  | 'PTL'
+  | 'PTC'
+  | 'PMC'
+  | 'PTR'
+  | 'PML'
+  | 'PMR'
+  | 'PBL'
+  | 'PBC'
+  | 'PBR'
+  | 'BMSG';
+
+export interface WidgetSetting {
+  name?: string;
+  type: PositionType;
+  show?: boolean;
+  visiable?: boolean;
+  position?: {
+    left?: number;
+    right?: number;
+    top?: number;
+    bottom?: number;
+  };
+  style?: {
+    width?: string | number;
+    maxWidth?: string;
+    height?: string | number;
+    [k: string]: any;
+  };
+}
