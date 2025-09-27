e77d9b3e90fc4caab3669a75c3d99df269c9ffe5
lanbery <lanbery@gmail.com>
2025-09-28 07:03:05 +0800

    feat: update

--- src/types/game.types.ts
+++ src/types/game.types.ts
@@ -10,18 +10,3 @@ export interface GameBase {
   intro?: string;
   rounds?: string;
 }
-
-/**
- * liveid
- * gameStartTime 比赛日期格式 yyyy-MM-dd HH:mm
- * isCumulatived 是否累计阶段时间
- * consumedTime 已消耗时间 sconds
- * stageStarting 时间毫秒值
- */
-export interface GameTiming {
-  liveid: string;
-  isCumulatived?: boolean;
-  gameStartTime: string;
-  consumedTime: number;
-  stageStarting: number;
-}
