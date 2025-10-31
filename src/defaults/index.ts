902664fe07aeb2933fdcb838e5b69072c4a096d6
lanbery <cwd-pc@xy-info.com>
2025-10-31 20:16:41 +0800

    refactor: kit settings

--- src/defaults/index.ts
+++ src/defaults/index.ts
@@ -1,1 +1,2 @@
 export * as FootballDefaults from './football.constants';
+export * as KitDefaults from './kit.defaults.contants';
---
+++ src/defaults/kit.defaults.contants.ts
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+
+import { RightTopTimingScoresCssMap } from '../types';
+
+export const defaultTimingScores: RightTopTimingScoresCssMap = {
+  top: 10,
+  right: 10,
+  height: 56,
+  fontSize: 16,
+  width: '188',
+  timing: {
+    width: 56,
+    color: '#FFF',
+    fontSize: 14,
+  },
+  home: {
+    fontColor: '#FFF',
+    teamColor: 'red',
+  },
+  away: {
+    fontColor: '#000',
+    teamColor: '#FFF',
+  },
+};
